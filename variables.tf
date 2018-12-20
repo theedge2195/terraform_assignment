20dd6fb6e980f6b66a03d18a5adfbc33c8ec5f7d
theedge2195 <haydencardwell21@gmail.com>
2018-12-19 20:01:18 -0600

    Delete variables.tf

--- variables.tf
+++
@@ -1,8 +0,0 @@
-variable "aws_region" {}
-variable "aws_profile" {}
-variable "vpc_cidr" {}
-variable "db_instance_class" {}
-variable "dbname" {}
-variable "dbuser" {}
-variable "dbpassword" {}
-variable "availability_zones" {}
