cc5c091543a20877635025ab746c1a795e29382f
theedge2195 <haydencardwell21@gmail.com>
2018-12-20 01:11:15 -0600

    Update variables2.tf

--- variables2.tf
+++ variables2.tf
@@ -13,3 +13,7 @@ variable "elb_healthy_threshold" {}
 variable "elb_unhealthy_threshold" {}
 variable "elb_timeout" {}
 variable "elb_interval" {}
+variable "key_name" {}
+variable "public_key_path" {}
+variable "dev_instance_type" {}
+variable "dev_ami" {}
