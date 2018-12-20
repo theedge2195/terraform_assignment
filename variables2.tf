04764258fd3b0cb993249b0dcb61f46755dfd28d
theedge2195 <haydencardwell21@gmail.com>
2018-12-20 00:03:45 -0600

    Update variables2.tf

--- variables2.tf
+++ variables2.tf
@@ -9,3 +9,7 @@ variable "db_instance_class" {}
 variable "dbname" {}
 variable "dbuser" {}
 variable "dbpassword" {}
+variable "elb_healthy_threshold" {}
+variable "elb_unhealthy_threshold" {}
+variable "elb_timeout" {}
+variable "elb_interval" {}
